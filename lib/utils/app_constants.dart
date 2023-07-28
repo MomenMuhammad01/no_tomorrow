9ae4f7b2f457ece4d2cea99494257552d57e57c8
Momen Muhammad <momenmuhammad01@gmail.com>
2023-07-28 21:10:06 +0200

    Routes added

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -1,3 +1,5 @@
 const String todoDataBaseName = 'Main_box';
 const String themeDataBaseName = 'Theme_box';
 const String themeDataBaseKey = 'Theme_Key';
+const String splashRoute = '/';
+const String homeRoute = '/home';
