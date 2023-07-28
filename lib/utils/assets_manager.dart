52c17f5ce2a4feef64f4fef65e1b3ca90387b252
Momen Muhammad <momenmuhammad01@gmail.com>
2023-07-28 21:08:33 +0200

    assets impl

---
+++ lib/utils/assets_manager.dart
@@ -0,0 +1,5 @@
+class AssetsManager{
+    static const fontFamily = 'OpenSans';
+    static const darkLogo = 'assets/images/dark_splash.png';
+    static const lightLogo = 'assets/images/light_splash.png';
+}
